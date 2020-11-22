49b8f944a61a8082733482bd64d33a09c49cde21
YenR <yen.0001@gmail.com>
2020-11-22 17:08:34 +0100

    replace scripts and make options menu
    Signed-off-by: YenR <yen.0001@gmail.com>

---
+++ GSE_Project/GSE Mini Game Jam project/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public void setVolume(float volume)
+    {
+        Debug.Log("new volume " + volume);
+    }
+
+    public void setFullscreen(bool isFullscreen)
+    {
+        Debug.Log("Fullscreen: " + isFullscreen);
+        Screen.fullScreen = isFullscreen;
+    }
+}
