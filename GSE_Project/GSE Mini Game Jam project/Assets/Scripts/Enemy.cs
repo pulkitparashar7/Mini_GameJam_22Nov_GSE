3da587c74fca0266872cf75facdc1893a62c5d46
pulkitp7 <pulkitp7@users.noreply.github.com>
2020-11-22 21:03:50 +0530

    working enemy with collision

--- GSE_Project/GSE Mini Game Jam project/Assets/Scripts/Enemy.cs
+++ GSE_Project/GSE Mini Game Jam project/Assets/Scripts/Enemy.cs
@@ -1,13 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+
+    public EnemyType EnemyType;
+    public float MySpeed;
+    public int NoPillNeeded = 1;
+    public float MovementDelay;
+    Image MyImage;
+    RectTransform MyRectTransform;
+    void Awake()
     {
-        
+        MyImage = GetComponent<Image>();
+        MyRectTransform = GetComponent<RectTransform>();
+    }
+
+    public void Initialize(EnemyType type, float speed)
+    {
+        EnemyType = type;
+        MySpeed = speed;
+        InvokeRepeating("MoveEnemy", MovementDelay / 2, MovementDelay);
+
+        switch(type)
+        {
+            case EnemyType.Green:
+                MyImage.color = Color.green;
+                break;
+            case EnemyType.Yellow:
+                MyImage.color = Color.yellow;
+                break;
+            case EnemyType.Red:
+                MyImage.color = Color.red;
+                break;
+        }
+    }
+
+    void MoveEnemy()
+    {
+        MyRectTransform.anchoredPosition = new Vector2(MyRectTransform.anchoredPosition.x + MySpeed, 0);
     }
 
     // Update is called once per frame
--- GSE_Project/GSE Mini Game Jam project/Assets/Scripts/EnemyController.cs
+++ GSE_Project/GSE Mini Game Jam project/Assets/Scripts/EnemyController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum EnemyTypes
+public enum EnemyType
 {
     Green = 0,
     Yellow = 1,
@@ -13,25 +13,49 @@ public class EnemyController : MonoBehaviour
 {
     public List<GameObject> SpawnPoints;
     public Enemy EnemyPrefab;
+    public float InitialSpawnDelay;
+    public float SpawnDelayInterval;
+    public List<Enemy> ActiveEnemies;
+
+    public Vector2 MovementDelay;
+    public float GreenPatientSpeed;
+    public float YellowPatientSpeed;
+    public float RedPatientSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-       
+        InitializeEnemies();
     }
 
     void InitializeEnemies()
     {
-        StartCoroutine(StartSpawningEnemies());
+        // StartCoroutine(StartSpawningEnemies());
+        InvokeRepeating("StartSpawningEnemies", InitialSpawnDelay, SpawnDelayInterval);
     }
 
-    IEnumerator StartSpawningEnemies()
+    int mLastUsedSpawnPoint = 0;
+    void StartSpawningEnemies()
     {
-        yield return null;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        int spawnPointID = Random.Range(0, SpawnPoints.Count);
+        if (mLastUsedSpawnPoint == spawnPointID)
+        {
+            spawnPointID = Random.Range(0, SpawnPoints.Count);
+        }
+        
+        var enemy = Instantiate(EnemyPrefab, SpawnPoints[spawnPointID].transform);
+        ActiveEnemies.Add(enemy);
+        enemy.MovementDelay = Random.Range(MovementDelay.x, MovementDelay.y);
+ 
+        int SpawnPatient = Random.Range(0, 3);
+        
+        if (SpawnPatient == 0)
+            enemy.Initialize(EnemyType.Green, -GreenPatientSpeed);
+        else if (SpawnPatient == 1)
+            enemy.Initialize(EnemyType.Yellow, -YellowPatientSpeed);
+        else if (SpawnPatient == 2)
+            enemy.Initialize(EnemyType.Red, -RedPatientSpeed);
         
     }
+
 }
