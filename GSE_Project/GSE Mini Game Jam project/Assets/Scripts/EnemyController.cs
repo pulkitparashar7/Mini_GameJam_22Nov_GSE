94b46b6dcc32e404cd3834a95025536898e94958
pulkitp7 <pulkitp7@users.noreply.github.com>
2020-11-22 18:02:12 +0530

    Added gitignore and Enemy AI

---
+++ GSE_Project/GSE Mini Game Jam project/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyTypes
+{
+    Green = 0,
+    Yellow = 1,
+    Red = 2
+
+}
+public class EnemyController : MonoBehaviour
+{
+    public List<GameObject> SpawnPoints;
+    public Enemy EnemyPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    void InitializeEnemies()
+    {
+        StartCoroutine(StartSpawningEnemies());
+    }
+
+    IEnumerator StartSpawningEnemies()
+    {
+        yield return null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
