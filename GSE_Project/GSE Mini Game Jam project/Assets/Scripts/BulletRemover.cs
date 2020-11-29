fd63a11c815ddaf62c70c6628094348c0286541a
StefanHohnwald <stefan.hohnwald@gmail.com>
2020-11-29 15:11:48 +0100

    Added hitzone to remove stray bullets

---
+++ GSE_Project/GSE Mini Game Jam project/Assets/Scripts/BulletRemover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletRemover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D hit)
+    {
+        GameObject otherObj = hit.gameObject;
+        MedicineBullet bullet = otherObj.GetComponent<MedicineBullet>();
+        if (bullet)
+        {
+            Destroy(bullet.gameObject);
+        }
+    }
+}
